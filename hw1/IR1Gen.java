078efd93ab293a4c5c7e31f91f9bb71552fd716f
kathtran <kathleenvdt@gmail.com>
2016-01-24 01:16:15 -0800

    fixing print method

--- hw1/IR1Gen.java
+++ hw1/IR1Gen.java
@@ -298,18 +298,21 @@ public class IR1Gen {
         List<IR1.Inst> code = new ArrayList<IR1.Inst>();
         List<IR1.Src> src = new ArrayList<IR1.Src>();
 
-        CodePack arg = gen(n.arg);
-        code.addAll(arg.code);
-        if (arg.src instanceof IR1.StrLit) {
-            src.add(arg.src);
+        if (n.arg != null) {
+            CodePack arg = gen(n.arg);
+            code.addAll(arg.code);
+            if (arg.src instanceof IR1.StrLit) {
+                src.add(arg.src);
+                code.add(new IR1.Call(new IR1.Global("_printStr"), src));
+            } else if (arg.src instanceof IR1.IntLit || arg.src instanceof IR1.BoolLit) {
+                src.add(arg.src);
+                code.add(new IR1.Call(new IR1.Global("_printInt"), src));
+            } else {
+                src.add(arg.src);
+                code.add(new IR1.Call(new IR1.Global("_print"), src));
+            }
+        } else
             code.add(new IR1.Call(new IR1.Global("_printStr"), src));
-        } else if (arg.src instanceof IR1.IntLit || arg.src instanceof IR1.BoolLit) {
-            src.add(arg.src);
-            code.add(new IR1.Call(new IR1.Global("_printInt"), src));
-        } else {
-            src.add(arg.src);
-            code.add(new IR1.Call(new IR1.Global("_print"), src));
-        }
 
         return code;
     }
