a00faa29127bceb24262c90b3e8dc9a8abd563e4
Kathleen Tran <kathtran@ada.cs.pdx.edu>
2016-01-24 18:44:00 -0800

    cleaned up Print

--- hw1/IR1Gen.java
+++ hw1/IR1Gen.java
@@ -304,13 +304,11 @@ public class IR1Gen {
         if (n.arg != null) {
             CodePack arg = gen(n.arg);
             code.addAll(arg.code);
-            if (arg.src instanceof IR1.StrLit) {
-                src.add(arg.src);
+            src.add(arg.src);
+            if (arg.src instanceof IR1.StrLit)
                 code.add(new IR1.Call(new IR1.Global("_printStr"), src));
-            } else {
-                src.add(arg.src);
+            else
                 code.add(new IR1.Call(new IR1.Global("_printInt"), src));
-            }
         } else
             code.add(new IR1.Call(new IR1.Global("_printStr"), src));
 
