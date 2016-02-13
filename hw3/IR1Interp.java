3e56489641bbf761712bf7d79481064343f12fec
kathtran <kathleenvdt@gmail.com>
2016-02-12 16:53:11 -0800

    set up labelMap

--- hw3/IR1Interp.java
+++ hw3/IR1Interp.java
@@ -3,16 +3,15 @@
 //---------------------------------------------------------------------------
 // For CS322 W'16 (J. Li).
 //
-
-// IR1 interpreter.
-//
+// Kathleen Tran
 //
 
-import ir.IR1;
-
-import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
+// IR1 interpreter. (A starter version)
+//
+//
+import java.util.*;
+import java.io.*;
+import ir.*;
 
 public class IR1Interp {
 
@@ -20,37 +19,29 @@ public class IR1Interp {
     public IntException(String msg) { super(msg); }
   }
 
-  // Value representation
+  //-----------------------------------------------------------------
+  // Value Representation
+  //-----------------------------------------------------------------
   //
-  abstract static class Val {
-    static final UndVal Undefined = new UndVal(); 
-    int asInt() throws Exception { 
-      throw new IntException("Integer value expected"); 
-    }
-    boolean asBool() throws Exception { 
-      throw new IntException("Boolean value expected"); 
-    }
-  }
+  abstract static class Val {}
 
-  // -- integer values
+  // -- Integer values
   //
   static class IntVal extends Val {
     int i;
     IntVal(int i) { this.i = i; }
-    int asInt() throws Exception { return i; }
     public String toString() { return "" + i; }
   }
 
-  // -- boolean values
+  // -- Boolean values
   //
   static class BoolVal extends Val {
     boolean b;
     BoolVal(boolean b) { this.b = b; }
-    boolean asBool() throws Exception { return b; }
     public String toString() { return "" + b; }
   }
 
-  // -- string values
+  // -- String values
   //
   static class StrVal extends Val {
     String s;
@@ -58,48 +49,52 @@ public class IR1Interp {
     public String toString() { return s; }
   }
 
-  // -- a special "undefined" value
+  // -- A special "undefined" value
   //
   static class UndVal extends Val {
     public String toString() { return "UndVal"; }
   }
 
-  // Environment representation
+  //-----------------------------------------------------------------
+  // Storage Organization
+  //-----------------------------------------------------------------
   //
-  static class Env {
-    HashMap<String, Integer> labelMap;  // label-to-idx map
-    HashMap<String, Val> varMap;        // var/temp-to-val map
 
-    Env() { 
-      labelMap = new HashMap<String, Integer>();
-      varMap = new HashMap<String, Val>();
-    }
-    public String toString() {
-      return "Env: labels" + labelMap + " vars" + varMap;
-    }
-  }
+  // -- Global heap memory
+  //
+  static ArrayList<Val> memory;
 
-  static HashMap<String,IR1.Func> funcMap; 	// func table
-  static ArrayList<Val> storage;		// heap memory
-  static final int CONTINUE = -1;		// execution status
-  static final int RETURN = -2;			// execution status
-  static Val retVal; 				// return value
+  // -- Environment for tracking var, temp, and param's values
+  //    (one copy per fuction invocation)
+  //
+  static class Env extends HashMap<String,Val> {}
 
-  static boolean statFlag = false;		// for reporting statistics
-  static int instCnt = 0;
+  //-----------------------------------------------------------------
+  // Other Data Structures
+  //-----------------------------------------------------------------
+  //
+  // GUIDE:
+  //  You have control over these. Either define look-up tables for 
+  //  functions and labels, or searching functions.
+  //
+  static class LabMap extends HashMap<String, Val> {}
+  static HashMap<String, LabMap> labelMap;
 
-  // The main method
+  // -- Useful global variables
+  //
+  static final int CONTINUE = -1;	// execution status 
+  static final int RETURN = -2;		// execution status
+  static Val retVal = null;             // for return value passing
+
+
+  //-----------------------------------------------------------------
+  // The Main Method
+  //-----------------------------------------------------------------
   //
   public static void main(String [] args) throws Exception {
-    if (args.length >= 1) {
-      FileInputStream stream;
-      if (args[0].equals("-v")) {
-	stream = new FileInputStream(args[1]);
-	statFlag = true;
-      }	else {
-	stream = new FileInputStream(args[0]);
-      }
-      IR1.Program p = new ir.IR1Parser(stream).Program();
+    if (args.length == 1) {
+      FileInputStream stream = new FileInputStream(args[0]);
+      IR1.Program p = new IR1Parser(stream).Program();
       stream.close();
       IR1Interp.execute(p);
     } else {
@@ -107,44 +102,39 @@ public class IR1Interp {
     }
   }
 
-  static int storageAlloc(int size) {
-    int loc = storage.size();
-    for (int i=0; i<size; i++)
-      storage.add(Val.Undefined);
-    return loc;
-  }
+  //-----------------------------------------------------------------
+  // Top-Level Nodes
+  //-----------------------------------------------------------------
+  //
 
   // Program ---
-  // Func[] funcs;
+  //  Func[] funcs;
+  //
+  // GUIDE:
+  // 1. Establish the look-up tables (if you plan to use them).
+  // 2. Look up or search for function '_main'.
+  // 3. Start interpreting from '_main' with an empty Env.
   //
   public static void execute(IR1.Program n) throws Exception { 
-    funcMap = new HashMap<String,IR1.Func>();
-    storage = new ArrayList<Val>();
-    retVal = Val.Undefined;
-    for (IR1.Func f: n.funcs)
-      funcMap.put(f.gname.s, f);
-    execute(funcMap.get("_main"), new Env());
-    if (statFlag)
-      System.out.println("# Total instructions executed: " + instCnt);
+
+    // ... code needed ...
+
   }
 
   // Func ---
-  // String name;
-  // Id[] params;
-  // Id[] locals;
-  // Inst[] code;
+  //  Global gname;
+  //  Id[] params;
+  //  Id[] locals;
+  //  Inst[] code;
+  //
+  // GUIDE:
+  //  - Implement the fetch-execute loop.
+  //  - The parameter 'env' is the function's initial Env, which
+  //    contains its parameters' values.
   //
   static void execute(IR1.Func n, Env env) throws Exception { 
-    for (int i=0; i<n.code.length; i++) {
-      if (n.code[i] instanceof IR1.LabelDec)
-	env.labelMap.put(((IR1.LabelDec) n.code[i]).lab.name, i);
-    }
-    // the fetch-execute loop
-    //  each execute() call returns CONTINUE, RETURN, 
-    //  or a new idx (target of jump)
     int idx = 0;
     while (idx < n.code.length) {
-      instCnt++;
       int next = execute(n.code[idx], env);
       if (next == CONTINUE)
 	idx++; 
@@ -155,8 +145,8 @@ public class IR1Interp {
     }
   }
 
-  // INSTRUCTIONS
-
+  // Dispatch execution to an individual Inst node.
+  //
   static int execute(IR1.Inst n, Env env) throws Exception {
     if (n instanceof IR1.Binop)    return execute((IR1.Binop) n, env);
     if (n instanceof IR1.Unop) 	   return execute((IR1.Unop) n, env);
@@ -171,59 +161,27 @@ public class IR1Interp {
     throw new IntException("Unknown Inst: " + n);
   }
 
+  //-----------------------------------------------------------------
+  // Individual Instruction Nodes
+  //-----------------------------------------------------------------
+  //
+  // - Each execute() routine returns CONTINUE, RETURN, or a new idx 
+  //   (target of jump).
+  //
+
   // Binop ---
   //  BOP op;
   //  Dest dst;
   //  Src src1, src2;
   //
+  // GUIDE:
+  // 1. Evaluate the operands, then perform the operation.
+  // 2. Update 'dst's entry in the Env with operation's result.
+  //
   static int execute(IR1.Binop n, Env env) throws Exception {
-    Val lval = evaluate(n.src1, env);
-    Val rval = evaluate(n.src2, env);
-    Val res = null;
-    if (lval instanceof IntVal && rval instanceof IntVal) {
-      int l = lval.asInt();
-      int r = rval.asInt();
-      if (n.op instanceof IR1.AOP) {
-	IR1.AOP op = (IR1.AOP) n.op;
-	switch (op) {
-	case ADD: res = new IntVal(l + r); break; 
-	case SUB: res = new IntVal(l - r); break; 
-	case MUL: res = new IntVal(l * r); break; 
-	case DIV: res = new IntVal(l / r); break; 
-	default:
-	  throw new IntException("Bad AOP in Binop: " + op);
-	}
-      } else if (n.op instanceof IR1.ROP) {
-	IR1.ROP op = (IR1.ROP) n.op;
-	switch (op) {
-	case EQ: res = new BoolVal(l == r); break;
-	case NE: res = new BoolVal(l != r); break;
-	case LT: res = new BoolVal(l < r); break;
-	case LE: res = new BoolVal(l <= r); break;
-	case GT: res = new BoolVal(l > r); break;
-	case GE: res = new BoolVal(l >= r); break;
-	default:
-	  throw new IntException("Bad ROP in Binop: " + op);
-	}
-      }
-    } else if (lval instanceof BoolVal && rval instanceof BoolVal) {
-      boolean l = lval.asBool();
-      boolean r = rval.asBool();
-      if (n.op instanceof IR1.AOP) {
-	IR1.AOP op = (IR1.AOP) n.op;
-	switch (op) {
-	case AND: res = new BoolVal(l && r); break;
-	case OR:  res = new BoolVal(l || r); break;
-	default:
-	  throw new IntException("Logic OP expected in Binop: " + op);
-	}
-      } else {
-	throw new IntException("AOP expected in Binop: " + n.op);
-      }
-    } else {
-      throw new IntException("Bad operands in Binop: " + n);
-    }
-    env.varMap.put(n.dst.toString(), res);
+
+    // ... code needed ...
+
     return CONTINUE;  
   }
 
@@ -232,16 +190,14 @@ public class IR1Interp {
   //  Dest dst;
   //  Src src;
   //
+  // GUIDE:
+  // 1. Evaluate the operand, then perform the operation.
+  // 2. Update 'dst's entry in the Env with operation's result.
+  //
   static int execute(IR1.Unop n, Env env) throws Exception {
-    Val val = evaluate(n.src, env);
-    Val res;
-    if (n.op == IR1.UOP.NEG)
-      res = new IntVal(-val.asInt());
-    else if (n.op == IR1.UOP.NOT)
-      res = new BoolVal(!val.asBool());
-    else
-      throw new IntException("Wrong op in Unop inst: " + n.op);
-    env.varMap.put(n.dst.toString(), res);
+
+    // ... code needed ...
+
     return CONTINUE;  
   }
 
@@ -249,34 +205,45 @@ public class IR1Interp {
   //  Dest dst;
   //  Src src;
   //
+  // GUIDE:
+  //  Evaluate 'src', then update 'dst's entry in the Env.
+  //
   static int execute(IR1.Move n, Env env) throws Exception {
-    Val val = evaluate(n.src, env);
-    env.varMap.put(n.dst.toString(), val);
-    return CONTINUE;
+
+    // ... code needed ...
+
+    return CONTINUE;  
   }
 
   // Load ---  
   //  Dest dst;
   //  Addr addr;
   //
+  // GUIDE:
+  //  Evaluate 'addr' to a memory index, then retrieve the stored 
+  //  value from memory and update 'dst's entry in the Env.
+  //
   static int execute(IR1.Load n, Env env) throws Exception {
-    int loc = evaluate(n.addr, env);
-    Val val = storage.get(loc);
-    if (val == null)
-      throw new IntException("No value at storage location " + loc);
-    env.varMap.put(n.dst.toString(), val);
-    return CONTINUE;
+
+    // ... code needed ...
+
+    return CONTINUE;  
   }
 
   // Store ---  
   //  Addr addr;
   //  Src src;
   //
+  // GUIDE:
+  // 1. Evaluate 'src' to a value.
+  // 2. Evaluate 'addr' to a memory index, then store the value
+  //    to the memory entry.
+  //
   static int execute(IR1.Store n, Env env) throws Exception {
-    Val val = evaluate(n.src, env);
-    int loc = evaluate(n.addr, env);
-    storage.set(loc, val);
-    return CONTINUE;
+
+    // ... code needed ...
+
+    return CONTINUE;  
   }
 
   // CJump ---
@@ -284,46 +251,31 @@ public class IR1Interp {
   //  Src src1, src2;
   //  Label lab;
   //
+  // GUIDE:
+  // 1. Evaluate the cond op.
+  // 2. If cond is true, find and return the instruction index 
+  //    of the jump target label; otherwise return CONTINUE.
+  //
   static int execute(IR1.CJump n, Env env) throws Exception {
-    boolean cond;
-    Val lval = evaluate(n.src1, env);
-    Val rval = evaluate(n.src2, env);
-    if (lval instanceof IntVal && rval instanceof IntVal) {
-      int l = lval.asInt();
-      int r = rval.asInt();
-      switch (n.op) {
-      case EQ: cond = (l == r); break;
-      case NE: cond = (l != r); break;
-      case LT: cond = (l < r); break;
-      case LE: cond = (l <= r); break;
-      case GT: cond = (l > r); break;
-      case GE: cond = (l >= r); break;
-      default:
-	throw new IntException("Wrong op in CJump: " + n.op);
-      }
-    } else if (lval instanceof BoolVal && rval instanceof BoolVal) {
-      boolean l = lval.asBool();
-      boolean r = rval.asBool();
-      switch (n.op) {
-      case EQ: cond = (l == r); break;
-      case NE: cond = (l != r); break;
-      default:
-	throw new IntException("Wrong op in CJump: " + n.op);
-      }
-    } else {
-      throw new IntException("Error in CJump: " + n);
-    }
-    if (cond)
-      return env.labelMap.get(n.lab.name);
-    else
-      return CONTINUE;
+
+    // ... code needed ...
+
+    return 0;
+
   }	
 
   // Jump ---
   //  Label lab;
   //
+  // GUIDE:
+  //  Find and return the instruction index of the jump target label.
+  //
   static int execute(IR1.Jump n, Env env) throws Exception {
-    return env.labelMap.get(n.lab.name);
+
+    // ... code needed ...
+
+    return 0;
+
   }	
 
   // Call ---
@@ -331,74 +283,85 @@ public class IR1Interp {
   //  Src[] args;
   //  Dest rdst;
   //
+  // GUIDE:
+  // 1. Evaluate the arguments to values.
+  // 2. Create a new Env for the callee; pair function's parameter
+  //    names with arguments' values, and add them to the new Env.
+  // 3. Find callee's Func node and switch to execute it.
+  // 4. If 'rdst' is not null, update its entry in the Env with
+  //    the return value (should be avaiable in variable 'retVal').
+  //
   static int execute(IR1.Call n, Env env) throws Exception {
-    if (n.gname.s.equals("_printInt")) { 
-      assert(n.args != null && n.args.length==1);
-      Val val = evaluate(n.args[0], env);
-      System.out.println("" + val);
-    } else if (n.gname.s.equals("_printStr")) {
-      if (n.args == null || n.args.length == 0) {
-	System.out.println();
-      } else {
-	Val val = evaluate(n.args[0], env);
-	System.out.println("" + val);
-      }
-    } else if (n.gname.s.equals("_malloc")) {
-      assert(n.args != null);
-      int size = evaluate(n.args[0], env).asInt();
-      int loc = storageAlloc(size);
-      env.varMap.put(n.rdst.toString(), new IntVal(loc));
-    } else {
-      IR1.Func func = funcMap.get(n.gname.s);
-      Env newenv = new Env();
-      for (int i=0; i<func.params.length; i++) {
-	String pname = "" + func.params[i];
-	Val argval = evaluate(n.args[i], env);
-	newenv.varMap.put(pname, argval);
-      }	
-      execute(func, newenv);
-      env.varMap.put(n.rdst.toString(), retVal);
-    } 
+
+    for (IR1.Src arg : n.args)
+      evaluate(arg, env);
+
     return CONTINUE;
-  }
+  }	
 
   // Return ---  
   //  Src val;
   //
+  // GUIDE:
+  //  If 'val' is not null, set it to the variable 'retVal'.
+  // 
   static int execute(IR1.Return n, Env env) throws Exception {
+
     if (n.val != null)
       retVal = evaluate(n.val, env);
+
     return RETURN;
   }
+
+  //-----------------------------------------------------------------
+  // Address and Operand Nodes.
+  //-----------------------------------------------------------------
+  //
+  // - Each has an evaluate() routine.
+  //
 
   // Address ---
   //  Src base;  
   //  int offset;
   //
+  // GUIDE:
+  // 1. Evaluate 'base' to an integer, then add 'offset' to it.
+  // 2. Return the result (which should be an index to memory).
+  //
   static int evaluate(IR1.Addr n, Env env) throws Exception {
-    int loc = evaluate(n.base, env).asInt();
+    int loc = ((IntVal) evaluate(n.base, env)).i;
     return loc + n.offset;
   }
 
-  // OPERANDS
-
+  // Src Nodes 
+  //  -> Temp | Id | IntLit | BooLit | StrLit
+  //
+  // GUIDE:
+  //  In each case, the evaluate() routine returns a Val object.
+  //  - For Temp and Id, look up their value from the Env, wrap 
+  //    it in a Val and return.
+  //  - For the literals, wrap their value in a Val and return.
+  //
   static Val evaluate(IR1.Src n, Env env) throws Exception {
-    Val val = null;
-    if (n instanceof IR1.Temp 
-	|| n instanceof IR1.Id)   val = env.varMap.get(n.toString());
-    if (n instanceof IR1.IntLit)  val = new IntVal(((IR1.IntLit) n).i);
-    if (n instanceof IR1.BoolLit) val = new BoolVal(((IR1.BoolLit) n).b);
-    if (n instanceof IR1.StrLit)  val = new StrVal(((IR1.StrLit) n).s);
-    if (val==null) 
-	throw new IntException("Src '" + n + "' has no value");
-    return val;
+    if (n instanceof IR1.Temp)    return env.get(n.toString());
+    if (n instanceof IR1.Id)      return env.get(n.toString());
+    if (n instanceof IR1.IntLit)  return new IntVal(((IR1.IntLit) n).i);
+    if (n instanceof IR1.BoolLit) return new BoolVal(((IR1.BoolLit) n).b);
+    if (n instanceof IR1.StrLit)  return new StrVal(((IR1.StrLit) n).s);
+    return new UndVal();
   }
 
+  // Dst Nodes 
+  //  -> Temp | Id
+  //
+  // GUIDE:
+  //  For both cases, look up their value from the Env, wrap it
+  //  in a Val and return.
+  //
   static Val evaluate(IR1.Dest n, Env env) throws Exception {
-    Val val = env.varMap.get(n.toString());
-    if (val==null) 
-	throw new IntException("Dest '" + n + "' has no value");
-    return val;
+    if (env.containsKey(n))
+      return env.get(n.toString());
+    return new UndVal();
   }
 
 }
