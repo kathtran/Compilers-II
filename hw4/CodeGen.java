a10837f0e30ecdc5995d764d3c0d3051fee5b6af
Kathleen Tran <kathtran@ada.cs.pdx.edu>
2016-03-09 16:04:16 -0800

    refining code

--- hw4/CodeGen.java
+++ hw4/CodeGen.java
@@ -242,7 +242,7 @@ class CodeGen {
       to_reg(n.src1, tempReg1);
       to_reg(n.src2, tempReg2);
 
-      X86.Reg reg = new X86.Reg(tempReg1.r, X86.Size.B);
+      X86.Reg reg = X86.resize_reg(X86.Size.B, tempReg1);
 
       X86.emit2("cmp" + tempReg1.s.suffix, tempReg2, tempReg1);
 
@@ -269,7 +269,7 @@ class CodeGen {
           throw new GenException("Invalid ROP: " + op);
       }
 
-      X86.emit2("movzbl", reg, new X86.Reg(tempReg1.r, X86.Size.L));
+      X86.emit2("movzbl", reg, X86.resize_reg(X86.Size.L, tempReg1));
     } else
       throw new GenException("Invalid BOP: " + op);
 
